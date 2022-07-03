df4726e0e556fbb2af5ea36a2a88eab00e4f0932
Zyad Tarek <zelnegely@ejada.com>
2022-07-03 19:14:54 +0200

    End of section 3

--- Core/Entities/Product.cs
+++ Core/Entities/Product.cs
@@ -1,8 +1,15 @@
 namespace Core.Entities
 {
-    public class Product
+    public class Product : BaseEntity
     {
-        public int Id { get; set; }
         public string? Name { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public string? PictureUrl { get; set; }
+        public ProductType ProductType { get; set; } = new ProductType();
+        public int ProductTypeId { get; set; }
+        public ProductBrand ProductBrand { get; set; } = new ProductBrand();
+        public int ProductBrandId { get; set; }
+
     }
 }
