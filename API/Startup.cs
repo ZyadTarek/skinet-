c42d12fdb787dca937e37277b454e0322f85fc1d
Zyad Tarek <zelnegely@ejada.com>
2022-07-19 00:03:00 +0300

    End of section 6

--- API/Startup.cs
+++ API/Startup.cs
@@ -39,6 +39,12 @@ namespace API
             services.AddApplicationServices();
 
             services.AddSwaggerDocumentation();
+
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -56,6 +62,8 @@ namespace API
 
             app.UseStaticFiles();
 
+            app.UseCors("CorsPolicy");
+            
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
